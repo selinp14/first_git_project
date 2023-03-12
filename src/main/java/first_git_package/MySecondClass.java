a6ac36d241456e51a6dabd34ef4e1be9d4876d1d
Selin <h.selin.pelit.7@gmail.com>
2023-03-12 14:40:27 +0100

    updating the second class

--- src/main/java/first_git_package/MySecondClass.java
+++ src/main/java/first_git_package/MySecondClass.java
@@ -3,5 +3,6 @@ package first_git_package;
 public class MySecondClass {
     public static void main(String[] args) {
         System.out.println("This is my second class");
+        System.out.println("UPDATING EXISTING CODE");
     }
 }
